0760e4ebaca789499c094aa04a192889fa1e478d
ahazimeh01 <ali.h@echovalley.io>
2023-03-28 23:47:19 +0300

    40

--- web-dev-simplified/script.ts
+++ web-dev-simplified/script.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ log: ["query"] });
 
 async function main() {
   //   const user = await prisma.user.create({ data: { name: "Sally" } });
@@ -7,6 +7,44 @@ async function main() {
   //   const users = await prisma.user.findMany();
   //   console.log(users);
   await prisma.user.deleteMany();
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Kyle",
+  //     email: "Kyle@test.com",
+  //     age: 27,
+  //     userPreference: {
+  //       create: {
+  //         emailUpdates: true,
+  //       },
+  //     },
+  //   },
+  //   // include: {
+  //   //   userPreference: true,
+  //   // },
+  //   select: {
+  //     name: true,
+  //     userPreference: {
+  //       select: {
+  //         id: true,
+  //       },
+  //     },
+  //   },
+  // });
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "Kyle",
+        email: "Kyle@test.com",
+        age: 27,
+      },
+      {
+        name: "Sally",
+        email: "sally@test.com",
+        age: 32,
+      },
+    ],
+  });
+  console.log(users);
 }
 
 main()
